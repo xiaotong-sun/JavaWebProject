d9df11193a9335451963b0c8effaef925ef1ff49
TonyDon <1400615611@qq.com>
2021-11-12 19:07:21 +0800

    设计购物车

---
+++ src/main/java/com/example/book/web/servlet/shoppingTrolleyServlet.java
@@ -0,0 +1,13 @@
+package com.example.book.web.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/shopping/*")
+public class shoppingTrolleyServlet extends BaseServlet {
+    public void add(HttpServletRequest request, HttpServletResponse response) {
+
+
+    }
+}
